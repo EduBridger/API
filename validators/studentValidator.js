7297e3760b10d4c096da12ae766b9be6164d0eb8
Wise-mystic <wisdomkwawuvi23@gmail.com>
2024-11-21 12:15:54 +0000

    level

--- validators/studentValidator.js
+++ validators/studentValidator.js
@@ -8,7 +8,7 @@ export const studentSchema = Joi.object({
 
   course: Joi.string().required().max(255),
 
-  level: Joi.number().required().max(5),
+  level: Joi.string().required().max(255),
 
   role: Joi.string().required().max(255),
 
